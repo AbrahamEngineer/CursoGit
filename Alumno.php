27fa894494cc49309c9a3a1c66dcf3914e746c0a
Abraham Cruz <abrahamguillermoc8@gmail.com>
2022-03-16 17:24:31 -0500

    Operadores aritmeticos en Clase Alumno

--- Alumno.php
+++ Alumno.php
@@ -2,33 +2,13 @@
 
 class Alumno{
 
-    private $codigo;
-    private $nombre;
-    private $edad;
-    private $dni;
-
-    public function getCodigo(){
-        return $this->codigo;
-    }
-
-    public function setCodigo($codigo){
-        return $this->codigo = $codigo;
-    }
-
-    public function getNombre(){
-        return $this->nombre;
-    }
-
-    public function setNombre($nombre){
-        return $this->nombre = $nombre;
-    }
-    
-    public function getEdad(){
-        return $this->edad;
-    }
-
-    public function setEdad($edad){
-        return $this->edad = $edad;
+    public function operacionesAritmeticas($numero1, $numero2, $operador){
+        switch($operador){
+            case 'suma': return $numero1 + $numero2;
+            case 'resta': return $numero1 - $numero2;
+            case 'multi': return $numero1 * $numero2;
+            case 'div': return $numero1 / $numero2;
+        } 
     }
 
 }
